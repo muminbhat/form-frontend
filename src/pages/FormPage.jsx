2bd65fae7bdc1b23b5f975f7923a4ae307fdd2f1
Mumin Bhat <mumin.fayaz5050@gmail.com>
2023-11-05 15:50:29 +0530

    post api

---
+++ src/pages/FormPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostForm from '../components/PostForm'
+
+const FormPage = () => {
+  return (
+    <PostForm />
+  )
+}
+
+export default FormPage
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
